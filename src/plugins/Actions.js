1bf75ec51f5a1591877dd8ff1fa9c3df2343a10e
邱旭 <uxuip@outlook.com>
2022-10-17 13:31:50 +0800

    fix: 修改引用模块

--- src/plugins/Actions.js
+++ src/plugins/Actions.js
@@ -1,7 +1,6 @@
-import VueActions from 'vue-actions'
+import VueActions, { component, directive } from '@oit/vue-actions'
 import { addUserBehaviorRecord } from '@/api/common'
 import Vue from 'vue'
-import { component, directive } from 'vue-actions'
 
 Vue.use(VueActions, {
   onRecords(data, otherData) {
@@ -10,10 +9,7 @@ Vue.use(VueActions, {
       return
     }
     data.page = otherData
-    console.log(data.records)
     data.records = data.records.filter(({ actions }) => !!(actions.click + actions.duration))
-    console.log(data.records)
-    // if (!data.records.length) return
     addUserBehaviorRecord(data)
   }
 })
