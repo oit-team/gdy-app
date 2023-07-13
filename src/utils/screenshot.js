fe628cefa7c28f6483c412d924fbc4e3d48540d1
邱旭 <uxuip@outlook.com>
2023-07-13 15:20:18 +0800

    feat: 图片分享到微信与保存

--- src/utils/screenshot.js
+++ src/utils/screenshot.js
@@ -1,11 +1,6 @@
 import html2canvas from "html2canvas"
 
-export async function screenshot(target, filename) {
+export async function screenshot(target) {
   const canvas = await html2canvas(target)
-  const image = canvas.toDataURL('image/png')
-
-  const link = document.createElement('a')
-  link.href = image
-  link.download = filename || `${new Date().toLocaleString()}.png`
-  link.click()
+  return canvas.toDataURL('image/png')
 }
